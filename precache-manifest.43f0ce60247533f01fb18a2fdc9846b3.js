a7464a1412666a2be48061c61ed2f3bb60dd7a01
Alisha-bashir <bussinessmail1257@gmail.com>
2020-10-21 18:27:42 -0700

    Updates

---
+++ precache-manifest.43f0ce60247533f01fb18a2fdc9846b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b375ef16af95495847d823eb0dd9c3",
+    "url": "/human-rolodex/index.html"
+  },
+  {
+    "revision": "76cbee95edab0002b086",
+    "url": "/human-rolodex/static/css/main.bff06b53.chunk.css"
+  },
+  {
+    "revision": "f51307f622d575f1ce02",
+    "url": "/human-rolodex/static/js/2.7c3df29f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/human-rolodex/static/js/2.7c3df29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cbee95edab0002b086",
+    "url": "/human-rolodex/static/js/main.e9873b81.chunk.js"
+  },
+  {
+    "revision": "6c9674b7e7a8e2188ec8",
+    "url": "/human-rolodex/static/js/runtime-main.e47adc39.js"
+  }
+]);
